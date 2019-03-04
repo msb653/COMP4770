de072b7600b251ad45b629ee3e96db7c84c447a8
BruceMate1 <46759935+BruceMate1@users.noreply.github.com>
2019-03-04 14:50:56 -0330

    Create elapsedTime.js
    Will be called to show timeElapsed on level

---
+++ gameCode/client/elapsedTime.js
@@ -0,0 +1,21 @@
+//time elapsed function that calculated time spent during level in seconds 
+//will be DISPLAYED apon level completion
+//option can be displayed as part of HUD//
+//tested and works
+
+var startTime, endTime;
+
+function start() {
+  startTime = new Date();
+};
+
+function end() {
+  endTime = new Date();
+  var timeDiff = endTime - startTime; //in ms
+  // strip the ms
+  timeDiff /= 1000;
+
+  // get seconds 
+  var seconds = Math.round(timeDiff);
+  console.log(seconds + " seconds");
+}
